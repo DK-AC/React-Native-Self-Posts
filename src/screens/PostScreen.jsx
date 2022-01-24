b1a8d966296d3e7546fdd94cf00a3f2ad781fc3d
DK-AC <denkacaj@gmail.com>
2022-01-24 12:33:59 +0300

    post page styling

--- src/screens/PostScreen.jsx
+++ src/screens/PostScreen.jsx
@@ -1,9 +1,14 @@
 import React from "react";
-import {Button, StyleSheet, Text, View} from "react-native";
+import {Alert, Button, Image, ScrollView, StyleSheet, Text} from "react-native";
+import {DATA} from "../data";
+import {Theme} from "../theme";
 
 
 export const PostScreen = ({navigation, route}) => {
     const date = new Date(route.params.item.date).toLocaleDateString()
+    const postId = route.params.item.id
+    const post = DATA.find(i => i.id === postId)
+
 
     React.useLayoutEffect(() => {
         navigation.setOptions({
@@ -11,25 +16,52 @@ export const PostScreen = ({navigation, route}) => {
         });
     }, [navigation]);
 
-    return (
-        <View style={styles.center}>
-            <Text>{route.params.item.id}</Text>
-            <Button title={'Go back'} onPress={() => navigation.goBack()}/>
+    const removePostImgHandler = () => {
+        Alert.alert(
+            `Удалить`,
+            `Вы действительно хотите удалить пост?`,
+            [
+                {
+                    text: "Отмена",
+                    style: "cancel"
+                },
+                {text: "OK", onPress: () => console.log("OK Pressed")}
+            ]
+        );
+    }
 
-        </View>
+
+    return (
+        <ScrollView>
+            <Image style={styles.img} source={{uri: post.img}}/>
+            <Text style={styles.textCenter}>{post.text}</Text>
+            <Button color={Theme.Red_Color} title={'Delete'} onPress={removePostImgHandler}/>
+        </ScrollView>
+
     )
 }
 PostScreen.screenOptions = ({route}) => {
-    console.log(route)
     return {
         headerTitle: 'Пост от ' + new Date(route.params.item.date).toLocaleDateString()
     }
 }
 
 const styles = StyleSheet.create({
+
     center: {
         flex: 1,
         justifyContent: "center",
-        alignItems: "center"
-    }
+        margin: 10
+    },
+    img: {
+        width: '100%',
+        height: 200,
+        marginBottom: 10
+    },
+    textCenter: {
+        fontFamily: 'Open_Sans_Regular',
+        textAlign: "center",
+        marginBottom: 10
+    },
+
 })
--- src/theme.js
+++ src/theme.js
@@ -1,4 +1,6 @@
 export const Theme = {
     Main_Color: '#3949ab',
-    White_Color: '#ffffff'
+    White_Color: '#ffffff',
+    Red_Color: '#FF0000',
+    Grey_Color: '#696969',
 }
