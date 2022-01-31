2fd7da04bc7003547045516ca4ecf9d0fa142e69
DK-AC <denkacaj@gmail.com>
2022-01-31 17:55:41 +0300

    createPost static method

--- src/database.js
+++ src/database.js
@@ -28,4 +28,16 @@ export class DB {
             })
         })
     }
+
+    static createPost({text, date, booked, img}) {
+        return new Promise((resolve, reject) => {
+            db.transaction(tx => {
+                tx.executeSql(`INSERT INTO posts (text,date,booked,img) VALUES (?,?,?,?)`,
+                    [text, date, booked, img],
+                    (_, result) => resolve(result.insertId),
+                    (_, error) => reject(error)
+                )
+            })
+        })
+    }
 }
